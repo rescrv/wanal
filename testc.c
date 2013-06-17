cab002147343f59139d3831be9c0598248360178
Robert Escriva <me@robescriva.com>
2013-06-17 11:26:45 -0400

    Update wanal flags for C

---
+++ testc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int
+main(int argc, const char* argv[])
+{
+    int i;
+
+    for (i = 0; i < argc; ++i)
+    {
+        printf("%d: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
